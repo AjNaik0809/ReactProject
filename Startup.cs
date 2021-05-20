c959ebbcba680716e16a2a31684ad20cacb3a6dc
AjayNaik0809 <ajaynaik8497@gmail.com>
2021-05-20 16:21:13 +0530

    first app

--- Startup.cs
+++ Startup.cs
@@ -42,7 +42,6 @@ namespace dotEmployeeReactProject
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-
             }
 
             app.UseHttpsRedirection();
